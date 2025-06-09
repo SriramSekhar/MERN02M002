8cc7a93f57d11b5154fcd56c19e6380fc9b150f7
SriramSekhar <sriramsekhar@gmail.com>
2025-06-09 11:35:01 +0530

    done

---
+++ june/week1/day5/script.js
@@ -0,0 +1,218 @@
+// console.log("welcome")
+
+
+// let a=1
+// let A=2
+// console.log(a)
+// console.log(A)
+
+
+// let _="Qlith";
+// console.log(_)
+
+// let $="Qlith innovation";
+// console.log($)
+
+// let _name="y";
+// console.log(_name)
+
+// let 1name="pagal";
+// console.log(1name);
+
+// let $name="xyz"
+// console.log($name)
+
+// let nameofStudent="block"
+// console.log (nameofStudent)
+
+// let a=20
+// console.log(a)
+// console.log(typeof a)
+
+// let name="Qlith"
+// console.log(name, "type is ",typeof name)
+
+// let isPass=true
+// console.log(isPass)
+// console.log(typeof isPass)
+
+// let empty=undefined
+// console.log(empty)
+// console.log(typeof empty)
+
+// let nullValue=null
+// console.log(nullValue)
+// console.log(typeof nullValue)
+
+// let b
+// console.log(b)
+
+// let c=20
+// console.log(c)
+// console.log(typeof c)
+
+// let d=20
+// console.log(d)
+// console.log(typeof typeof d)
+
+
+// let d=30.5
+// console.log(typeof d)
+
+
+// global declaration of the variable
+// var fullName="Qlith innovation";
+// console.log(fullName)
+
+// {
+//     console.log(fullName)
+// }
+// function getName(){
+//     console.log(fullName)
+// }
+// getName()
+
+
+// normal block
+// this is my block level declaration of variable
+
+// {
+//     var a=40
+//     console.log("block level declaration of variable")
+// }
+
+// console.log("local access :",a)
+
+// function getA(){
+//     console.log("function level declaration",a)
+// }
+// getA();
+
+
+// function variableDeclare(){
+//     var r=50
+//     console.log(r)
+
+// }
+// variableDeclare()
+// console.log(r)
+
+// global declaration
+// let name="innovation"
+// console.log(name)
+// {
+//     console.log(name)
+
+// }
+// function getName(){
+//     console.log(name)
+// }
+// getName()
+
+// // block level declaration
+// {
+//     let a=30
+//     console.log(a)
+// }
+
+// console.log("local access :",a)
+
+// // function level declaration
+// function getA(){
+//     console.log("function level declaration",a)
+//     console.log(name)
+// }
+// getA()
+
+// global variable
+// const name
+
+// var a=60
+// console.log(a)
+
+//  a=40
+//  console.log(a)
+
+//  a="y"
+//  console.log(a)
+
+//  var a=30
+//  console.log(a)
+// let a=70
+// let a=60
+// console.log(a)
+
+// a=80
+// console.log(a)
+
+
+
+// ~ Differnece between var, let, const 
+
+// redeclartion :
+//    var =>possiable
+//    let =>not possiable
+//    const =>not
+
+// ReInitialization
+//    var =>possible
+//    let =>possiable
+//  const =>not possiable
+
+// Only Declaraion possiable or not
+//  var =>possiable
+//  let =>possiable
+//  const =>not possiable
+
+
+//& way of declaration  and Access
+
+// global declaration
+//  var
+// local access :var =>access
+// block access :access
+// function access :access
+
+// let 
+// local access : =>access
+// block access :access
+// function access :access
+
+// const 
+// local access : =>possiable
+// block access :access
+// function access :access
+
+
+// Block level declaration
+// var
+// local access : =>possiable
+// block access :access
+// function access :access
+
+// let 
+// local access : =>not possiable
+// block access :access
+// function access :not access
+
+// const 
+// local access : => not possiable
+// block access :access
+// function access :not access
+
+
+// function level declaratiotn
+// var 
+// local access : =>not possiable
+// block access :not possiable
+// function access :access 
+
+// let 
+// local access : =>not possiable
+// block access :not access
+// function access :access
+
+// const 
+// local access : =>not possiable
+// block access :not access
+// function access :access
